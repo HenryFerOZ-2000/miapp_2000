c341a30ea33d77ae2d4fe4cff7b2eb4f8bb8d1e2
Henry Ochoa <hfoz8a@gmail.com>
2025-07-06 22:00:54 -0500

    Actualización configuración staticfiles y nuevos archivos estáticos

--- miapp/wsgi.py
+++ miapp/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/tu_usuario/miapp_2000'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'miapp.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miapp.settings')
-
 application = get_wsgi_application()
